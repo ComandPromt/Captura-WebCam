27fc6e4125e3eb8d09a375a3f5b76e9ba43b24f9
ComandPromt <vayase_a_ferrol@hotmail.com>
2019-01-15 12:53:39 +0100

    Add files via upload

---
+++ solWebcam/solWebcam/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("jcWebcam")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("HP")> 
+<Assembly: AssemblyProduct("jcWebcam")> 
+<Assembly: AssemblyCopyright("Copyright © HP 2012")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("3e52ffd4-2e6a-4561-aaab-4ed12f172ac2")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
